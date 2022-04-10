7e465d1ca41b7db8080bfedf3aee0b03ef2697aa
Empereur-Hanibal <78930962+Empereur-Hanibal@users.noreply.github.com>
2022-04-10 20:27:06 +0200

    troisèmes essaie push

---
+++ src/Vue/Acceuil/FrameAccueil.java
@@ -0,0 +1,15 @@
+package Vue.Acceuil;
+
+import javax.swing.*;
+
+public class FrameAccueil extends JFrame
+{
+    public FrameAccueil()
+    {
+        this.setTitle("Acceuil");
+        this.setLocation(50,50);
+
+        this.pack();
+        this.setVisible(true);
+    }
+}
